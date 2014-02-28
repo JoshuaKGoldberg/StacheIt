8daad007be43eb8ba61b785fd24523b3ee59b6a6
Justin <jgwitkowski@gmail.com>
2014-02-28 14:42:25 -0500

    Basic authentication model created, database settings invoked in settings.py,  and requirements.txt updated

--- stacheit_project/authentication/models.py
+++ stacheit_project/authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+class Stacher(models.Model):
+	user = models.OneToOneField(User)
+	display_name = models.CharField(max_length=25)
+	email = models.EmailField(max_length=75)
+	date_created = models.DateTimeField(auto_now_add=True)
+
--- stacheit_project/stacheit_project/settings.py
+++ stacheit_project/stacheit_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_auth',
+    'authentication'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -56,9 +58,9 @@ WSGI_APPLICATION = 'stacheit_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default' : {
+        'ENGINE' : 'django_mongodb_engine',
+        'NAME' : 'stache'
     }
 }
 
