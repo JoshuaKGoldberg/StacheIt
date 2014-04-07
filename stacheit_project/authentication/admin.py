9271dbd1f4a84a27845d34b8a072af144eb41c12
Justin <jgwitkowski@gmail.com>
2014-04-07 18:11:23 -0400

    Initial Data Model for Annotations and Articles

--- stacheit_project/authentication/admin.py
+++ stacheit_project/authentication/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from authentication.models import Stacher
+from authentication.models import Stacher, Annotation, Article, StacherArticles, ArticleAnnotations
 
 # Register your models here.
 
+admin.site.register(Annotation)
 admin.site.register(Stacher)
+admin.site.register(Article)
+admin.site.register(ArticleAnnotations)
+admin.site.register(StacherArticles)
